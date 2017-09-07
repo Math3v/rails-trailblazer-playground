ca50bc4c917a4bea02c66ab6b56fb89de188faaa
Matej Minarik <mato.minarik@azet.sk>
2017-09-07 15:33:38 +0200

    Comment creation expects thing_id instead of thing object

--- app/concepts/comment/operation/create.rb
+++ app/concepts/comment/operation/create.rb
@@ -1,7 +1,12 @@
 class Comment::Create < Trailblazer::Operation
   class Present < Trailblazer::Operation
     step Model(Comment, :new)
+    step :assign_thing!
     step Contract::Build(constant: Comment::Contract::Create)
+
+    def assign_thing!(options, params:, **)
+      options['model'].thing = Thing.find_by_id(params[:thing_id]) || Thing.new
+    end
   end
 
   step Nested(Present)
--- spec/concepts/comment/operation/create_spec.rb
+++ spec/concepts/comment/operation/create_spec.rb
@@ -4,16 +4,16 @@ describe 'Comment::Create' do
 
   it 'creates valid Comment' do
     thing = ThingFactory.new.()
-    result = Comment::Create.call(comment: { body: 'Testing body', thing: thing})
+    result = Comment::Create.call(comment: { body: 'Testing body'}, thing_id: thing.id)
     expect(result.success?).to be(true)
     expect(result['contract.default'].errors.messages.length).to be(0)
     expect(Thing.first.comments.count).to be(1)
   end
   
   it 'cannot create Comment for invalid Thing' do 
-    result = Comment::Create.call(comment: {body: 'Testing body', thing: {}})
+    result = Comment::Create.call(comment: {body: 'Testing body'})
     expect(result.success?).to be(false)
-    expect(result['contract.default'].errors.messages.inspect).to eq('{:thing=>["must be filled", "Thing does not exist"]}')
+    expect(result['contract.default'].errors.messages.inspect).to eq('{:thing=>["Thing does not exist"]}')
   end
   
   it 'cannot create Comment for unpersisted Thing' do
